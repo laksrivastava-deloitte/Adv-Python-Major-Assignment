bc72b407d3647728a0e5697cb92fbf834e92b9a0
laksrivastava-deloitte <laksrivastava@deloitte.com>
2022-08-21 02:22:17 +0530

    test.py formated accoring to sonar lint

--- gateway_service/test.py
+++ gateway_service/test.py
@@ -1,9 +1,9 @@
-from glob import glob
 import unittest
 import requests
 import json
 
 url_gateway = "http://127.0.0.1:5004"
+#Global object
 header = {
     'Content-Type': 'application/json',
     'X-Access-Token': 'No Token'
@@ -14,9 +14,17 @@ test_item = ""
 test_promo=""
 test_cartitem=""
 test_payment=""
+
+#String part of url
 register = "/register"
 promo_name="super10"
-
+create_category="/createCategory"
+login='/login'
+newitem='/newItem'
+category='/categ'+'ory/'
+promodata='/promodata/'
+cartdata='/cartdata'
+item='/item/'
 
 def execute(request_type, url, header=header, data={}):
     response = requests.request(
@@ -36,7 +44,7 @@ class Test1_User(unittest.TestCase):
 
     def test_02_Login_user(self):
         global header
-        response = execute("POST", url_gateway+'/login', header, test_user)
+        response = execute("POST", url_gateway+login, header, test_user)
         self.assertEqual(response.status_code, 200)
         header['X-Access-Token']=response.json()['token']
         
@@ -48,7 +56,7 @@ class Test1_User(unittest.TestCase):
                            header, {'password': 'abcd'})
         self.assertEqual(response.status_code, 200)
         # relogin to get new token
-        response = execute("POST", url_gateway+'/login', header,
+        response = execute("POST", url_gateway+login, header,
                            {'username': test_user['username'], 'password': 'abcd'})
         self.assertEqual(response.status_code, 200)
         header['X-Access-Token']=response.json()['token']
@@ -67,7 +75,7 @@ class Test2_Product(unittest.TestCase):
         global header
         execute("POST", url_gateway+register, header, test_user)
         # self.assertEqual(response.status_code,201)
-        response = execute("POST", url_gateway+'/login', header, test_user)
+        response = execute("POST", url_gateway+login, header, test_user)
         self.assertEqual(response.status_code, 200)
         header['X-Access-Token']=response.json()['token']
 
@@ -75,12 +83,12 @@ class Test2_Product(unittest.TestCase):
         global test_category
         data = {"name": "Test Product"}
         # Create category
-        response = execute("POST", url_gateway+'/createCategory', header, data)
+        response = execute("POST", url_gateway+create_category, header, data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()['data']['name'], data['name'])
         test_category = response.json()['data']
         # Recreate same category
-        response = execute("POST", url_gateway+'/createCategory', header, data)
+        response = execute("POST", url_gateway+create_category, header, data)
         self.assertEqual(response.status_code, 406)
 
     def test_07_Create_item(self):
@@ -89,11 +97,11 @@ class Test2_Product(unittest.TestCase):
         data = {'name': 'Test Item', 'item_count': 100, "rating": 4,
                 "price": 10, "category_id": test_category['id']}
         # Create item
-        response = execute("POST", url_gateway+'/newItem', header, data)
+        response = execute("POST", url_gateway+newitem, header, data)
         self.assertEqual(response.status_code, 201)
         test_item = response.json()['data']
         # Recreate same item
-        response = execute("POST", url_gateway+'/newItem', header, data)
+        response = execute("POST", url_gateway+newitem, header, data)
         self.assertEqual(response.status_code, 406)
 
     def test_08_Update_item(self):
@@ -102,7 +110,7 @@ class Test2_Product(unittest.TestCase):
         data = {'name': test_item['name'],
                 "item_count": 700, "rating": 2, "price": 20}
         # Update item
-        response = execute("PUT", url_gateway+'/newItem', header, data)
+        response = execute("PUT", url_gateway+newitem, header, data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data']['price'], 20)
         self.assertEqual(response.json()['data']['rating'], 2)
@@ -114,7 +122,7 @@ class Test2_Product(unittest.TestCase):
     def test_09_Get_category(self):
         global test_category
         global test_item
-        response = execute("GET", url_gateway+'/category/' +
+        response = execute("GET", url_gateway+category +
                            str(test_category['id']), header)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
@@ -145,18 +153,18 @@ class Test2_Product(unittest.TestCase):
 
     def test_12_get_item(self):
         global test_item
-        response = execute("GET", url_gateway+'/item/'+str(test_item['id']), header)
+        response = execute("GET", url_gateway+item+str(test_item['id']), header)
         self.assertEqual(response.status_code,200)
         self.assertDictEqual(response.json()['item'],test_item)
 
     def test_13_Delete_item(self):
         global test_item
         # Delete item
-        response = execute("DELETE", url_gateway+'/item/' +
+        response = execute("DELETE", url_gateway+item +
                            test_item['name'], header)
         self.assertEqual(response.status_code, 200)
         # Redelete item
-        response = execute("DELETE", url_gateway+'/item/' +
+        response = execute("DELETE", url_gateway+item +
                            test_item['name'], header)
         self.assertEqual(response.status_code, 406)
 
@@ -186,20 +194,20 @@ class Test3_CashFlow(unittest.TestCase):
 
         #user create
         execute("POST", url_gateway+register, header, test_user)
-        response = execute("POST", url_gateway+'/login', header, test_user)
+        response = execute("POST", url_gateway+login, header, test_user)
         self.assertEqual(response.status_code, 200)
         header['X-Access-Token']=response.json()['token']
 
         # Create category
         data = {"name": "Test Product"}
-        response = execute("POST", url_gateway+'/createCategory', header, data)
+        response = execute("POST", url_gateway+create_category, header, data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()['data']['name'], data['name'])
         test_category = response.json()['data']
 
         # Create item
         data = {'name': 'Test Item', 'item_count': 100, "rating": 4,"price": 10, "category_id": test_category['id']}
-        response = execute("POST", url_gateway+'/newItem', header, data)
+        response = execute("POST", url_gateway+newitem, header, data)
         self.assertEqual(response.status_code, 201)
         test_item = response.json()['data']
 
@@ -207,19 +215,19 @@ class Test3_CashFlow(unittest.TestCase):
     def test_17_Create_promo(self):
         #create promo
         data={"min_amount":5,"max_amount":30,"percentage":0.02}
-        response = execute("POST", url_gateway+'/promodata/'+promo_name, header, data)
+        response = execute("POST", url_gateway+promodata+promo_name, header, data)
         self.assertEqual(response.json()['result']['name'],promo_name)
         self.assertEqual(response.json()['result']['min_amount'],5)
         self.assertEqual(response.json()['result']['max_amount'],30)
 
         #recreate promo
-        response = execute("POST", url_gateway+'/promodata/'+promo_name, header, data)
+        response = execute("POST", url_gateway+promodata+promo_name, header, data)
         self.assertEqual(response.status_code,406)
 
     def test_18_Update_promo(self):
         global test_promo
         data={"min_amount":10,"max_amount":40,"percentage":0.03}
-        response = execute("PUT", url_gateway+'/promodata/'+promo_name, header, data)
+        response = execute("PUT", url_gateway+promodata+promo_name, header, data)
         self.assertEqual(response.json()['result']['name'],promo_name)
         self.assertEqual(response.json()['result']['min_amount'],10)
         self.assertEqual(response.json()['result']['max_amount'],40)
@@ -242,7 +250,7 @@ class Test3_CashFlow(unittest.TestCase):
         global test_user
         global test_cartitem
         data={"product_id":test_item['id'],"quantity":5}
-        response = execute("POST", url_gateway+'/cartdata', header,data)
+        response = execute("POST", url_gateway+cartdata, header,data)
         self.assertEqual(response.status_code,201)
         self.assertEqual(response.json()['data']['category_id'],test_item['category_id'])
         self.assertEqual(response.json()['data']['product_id'],test_item['id'])
@@ -251,7 +259,7 @@ class Test3_CashFlow(unittest.TestCase):
 
     def test_21_All_Cart_items(self):
         global test_cartitem
-        response = execute("GET", url_gateway+'/cartdata', header)
+        response = execute("GET", url_gateway+cartdata, header)
         self.assertEqual(response.status_code,200)
         self.assertDictEqual(response.json()['cart items'][0],test_cartitem)
 
@@ -272,14 +280,14 @@ class Test3_CashFlow(unittest.TestCase):
     def test_24_Delete_all_cart_item(self):
         #Insert item in cart
         data={"product_id":test_item['id'],"quantity":5}
-        execute("POST", url_gateway+'/cartdata', header,data)
+        execute("POST", url_gateway+cartdata, header,data)
 
         #Delete all cart item
-        response = execute("DELETE", url_gateway+'/cartdata', header)
+        response = execute("DELETE", url_gateway+cartdata, header)
         self.assertEqual(response.status_code,200)
 
         #Re-Delete all cart item
-        response = execute("DELETE", url_gateway+'/cartdata', header)
+        response = execute("DELETE", url_gateway+cartdata, header)
         self.assertEqual(response.status_code,200)
 
     def test_25_Cart_checkout(self):
@@ -289,7 +297,7 @@ class Test3_CashFlow(unittest.TestCase):
 
         #Insert item in cart
         data={"product_id":test_item['id'],"quantity":5}
-        response=execute("POST", url_gateway+'/cartdata', header,data)
+        response=execute("POST", url_gateway+cartdata, header,data)
         test_cartitem=response.json()['data']
 
         #Cart Checkout
@@ -308,16 +316,16 @@ class Test3_CashFlow(unittest.TestCase):
 
     def test_27_Delete_promo(self):
         #delete promo
-        response = execute("Delete", url_gateway+'/promodata/'+promo_name, header)
+        response = execute("Delete", url_gateway+promodata+promo_name, header)
         self.assertEqual(response.status_code,200)
 
         #re-delete promo
-        response = execute("Delete", url_gateway+'/promodata/'+promo_name, header)
+        response = execute("Delete", url_gateway+promodata+promo_name, header)
         self.assertEqual(response.status_code,406)
 
     def test_28_User_category_item_end(self):
         # delete category
-        response = execute("DELETE", url_gateway +'/category/'+test_category['name'], header)
+        response = execute("DELETE", url_gateway +category+test_category['name'], header)
         self.assertEqual(response.status_code, 200)
 
         #user end
