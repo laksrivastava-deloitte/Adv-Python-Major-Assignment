82147b3da2e8fc164f91a76f6e2fe9e33fa2c0ee
laksrivastava-deloitte <laksrivastava@deloitte.com>
2022-08-21 23:43:50 +0530

    Swagger Completed

--- gateway_service/app.py
+++ gateway_service/app.py
@@ -1,18 +1,37 @@
-from flask import Flask
+from flask import Flask,jsonify,make_response,send_from_directory
+from flask_cors import CORS
+# from routes import request_api
+import argparse
 from flask_restful import Api
 import time
 from flask import g
-# from resources.cashflow 
 from resources.user import  UserData,UserList,UserLogin
 from resources.product import CategoryData,CategoryList,SingleCategory,ItemData,SingleItem,ItemList,ReduceItemCount
 from resources.cashflow import CartData,CartDataList,CartSingleItem,MakePayment,PaymentListUser
 from resources.promo import PromoData,PromoList
-# from resource.product
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 app = Flask(__name__)
 app.secret_key = 'Lakshya Srivastava Project'
 api = Api(app)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Advanced Python(Flask) present HashKart"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+# app.register_blueprint(request_api.get_blueprint())
 
 #Run on all request
 @app.before_request
